7f9e4e0c6bc1ebfdccfb2881691ed57d2f4de0d7
newli <newri0807@gmail.com>
2023-03-01 12:27:06 +0900

    3주차 과제 - 폴더 정리

--- src/components/Demo/Result.jsx
+++ src/components/Demo/Result.jsx
@@ -1,83 +1,11 @@
 import React from "react";
 import { DemoPie } from "../../data/pieChart";
 import { LoadingButton } from "@mui/lab";
+import { colorPick } from "../func/colorPick";
 
 export const Result = ({ dataList, loading }) => {
   const percentageCal = (percent) => {
     return `${(percent * 100).toFixed(0)}%`;
-  };
-
-  const colorPick = (stock, type) => {
-    if (type === "txt") {
-      switch (true) {
-        case ["SK하이닉스", "SK증권", "한국테크놀로지", "현대차"].includes(
-          stock
-        ):
-          return {
-            color: "rgb(99, 149, 249)",
-            fontWeight: "bold",
-            opacity: "1",
-          };
-        case ["SK", "엔비디아", "율호"].includes(stock):
-          return {
-            color: "rgb(98, 218, 171)",
-            fontWeight: "bold",
-            opacity: "1",
-          };
-        case ["한솔케미칼", "엔켐"].includes(stock):
-          console.log("한솔케미칼");
-          return {
-            color: "rgb(101, 119, 152)",
-            fontWeight: "bold",
-            opacity: "1",
-          };
-        case ["광무"].includes(stock):
-          return {
-            color: "rgb(99, 109, 249)",
-            fontWeight: "bold",
-            opacity: "1",
-          };
-        case ["서보산업"].includes(stock):
-          return {
-            color: "rgb(79, 9, 149)",
-            fontWeight: "bold",
-            opacity: "1",
-          };
-        default:
-          return { color: "#000" };
-      }
-    } else {
-      switch (true) {
-        case ["SK하이닉스", "SK증권", "한국테크놀로지", "현대차"].includes(
-          stock
-        ):
-          return {
-            backgroundColor: "rgba(99, 149, 249, 0.15)",
-          };
-        case ["SK", "엔비디아", "율호"].includes(stock):
-          return {
-            backgroundColor: "rgba(98, 218, 171, 0.15)",
-          };
-        case ["한솔케미칼", "엔켐"].includes(stock):
-          return {
-            backgroundColor: "rgba(101, 119, 152, 0.15)",
-          };
-        case ["광무"].includes(stock):
-          return {
-            backgroundColor: "rgba(99, 109, 249, 0.15)",
-          };
-        case ["율호"].includes(stock):
-          return {
-            backgroundColor: "rgba(79, 109, 249, 0.15)",
-          };
-        case ["서보산업"].includes(stock):
-          return {
-            backgroundColor: "rgba(79, 9, 149, 0.15)",
-          };
-        default:
-          return { backgroundColor: "rgb(245, 245, 245)" };
-      }
-    }
   };
 
   if (loading) return <LoadingButton loading variant="none" />;
